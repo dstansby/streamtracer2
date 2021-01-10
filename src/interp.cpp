658eb8da2f96d7e299925ae2a06b3ffb480772a4
David Stansby <dstansby@gmail.com>
2021-01-10 17:42:51 +0000

    Remove interp_interval

--- src/interp.cpp
+++ src/interp.cpp
@@ -2,24 +2,7 @@
 
 namespace interp
 {
-
-double interp_interval(double x, double xp[2], double fp[2])
-/*
-Given a sample point within an interval, and the value of a function at the
-interval end points, return a linear interpolation of the function.
-
-Parameters
-----------
-x : sample point
-xp : interval start and end points
-fp : funciton values at xp
-*/
-{
-  // Fraction along the interval
-  double frac = (x - xp[0]) / (xp[1] - xp[0]);
-  return fp[0] + frac * (fp[1] - fp[0]);
-}
-
+  
 double interp_trilinear(double x[3], double f[2][2][2])
 /*
 Perform trilinear interpolation within a normalised cube.
--- tests/test_interp.cpp
+++ tests/test_interp.cpp
@@ -2,15 +2,6 @@
 #include "../src/interp.h"
 
 
-TEST_CASE( "Factorials are computed", "[interp_interval]" ) {
-    double x = 1.0;
-    double xp[2] = {0.0, 2.0};
-    double fp[2] = {3.0, 5.0};
-
-    double interped = interp::interp_interval(x, xp, fp);
-    REQUIRE( interped == 4.0 );
-}
-
 TEST_CASE( "Trilinear interpolation", "[interp_trilinear]" ) {
     double interped;
     // Case with all zeros at corners
